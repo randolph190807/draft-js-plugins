87a3e9f0149ec8b24ed834a5a71a805624609229
randolph190807 <77778707+randolph190807@users.noreply.github.com>
2021-05-06 17:17:18 +0800

    修改mentionRegexp 支持@前可以添加字符

--- packages/mention/src/mentionSuggestionsStrategy.ts
+++ packages/mention/src/mentionSuggestionsStrategy.ts
@@ -103,7 +103,7 @@ export default (
     .join('|')})`;
   const MENTION_REGEX = supportWhiteSpace
     ? new RegExp(`${triggerPattern}(${regExp}|\\s)*`, 'g')
-    : new RegExp(`(\\s|^)${triggerPattern}${regExp}*`, 'g');
+    : new RegExp(`(\\w|^)${triggerPattern}${regExp}*`, 'g');
 
   return (contentBlock: ContentBlock, callback: FindWithRegexCb) => {
     findWithRegex(MENTION_REGEX, contentBlock, supportWhiteSpace, callback);
